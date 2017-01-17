eab599044bbcde5ef9e56fbb6e57daf4b7b2903d
Pete <lowep@itcarlow.ie>
2017-01-17 16:55:49 +0000

    removed stdfx.h lines

--- SFML Starter game/GamePlay.cpp
+++ SFML Starter game/GamePlay.cpp
@@ -1,5 +1,5 @@
 
-#include "stdafx.h"
+
 #include "GamePlay.h"
 #include "Game.h"
 #include <iostream>
--- SFML Starter game/HelpPage.cpp
+++ SFML Starter game/HelpPage.cpp
@@ -1,4 +1,4 @@
-#include "stdafx.h"
+
 #include "HelpPage.h"
 #include "Game.h"
 #include <iostream>
